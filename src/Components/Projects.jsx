b24c77b53d5d879313dc3aa7536cf3579f8edceb
SUNEEL KUMAR <122699505+formaxx786@users.noreply.github.com>
2024-09-29 08:23:30 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <div className=' border-b border-neutral-900 pb-4'>
          <motion.h2 whileInView={{opacity:1,y:0}} initial={{opacity:0,y:-80}} transition={{duration:1.2}} className='my-20 text-center text-4xl'>Projects</motion.h2>
-    <div >{PROJECTS.map((project,index)=>(
+     <div >{PROJECTS.map((project,index)=>(
            <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
             <div className='w-full lg:w-1/4'>
              <a href="https://to-do-manager-one.vercel.app/"><motion.img whileInView={{opacity:1,x:0}} initial={{opacity:0, x:-130}} transition={{duration:1.3}} src={project.image} alt='project.title' className='w-[200px] h-[230px] rounded-lg'/></a>
