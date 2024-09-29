0882900567673192280a00c8781a247c4f6601c8
SUNEEL KUMAR <122699505+formaxx786@users.noreply.github.com>
2024-09-29 11:28:41 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,9 +8,9 @@ function Projects() {
      <div >{PROJECTS.map((project,index)=>(
            <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
             <div className='w-full lg:w-1/4'>
-             <a href="https://to-do-manager-one.vercel.app/"><motion.img whileInView={{opacity:1,x:0}} initial={{opacity:0, x:-130}} transition={{duration:1.3}} src={project.image} alt='project.title' className='w-[200px] h-[230px] rounded-lg'/></a>
+             <a href="https://to-do-manager-one.vercel.app/"><motion.img whileInView={{opacity:1,x:0}} initial={{opacity:0, x:-130}} transition={{duration:1.2}} src={project.image} alt='project.title' className='w-[200px] h-[230px] rounded-lg'/></a>
            </div>
-      <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0, x:130}} transition={{duration:1.3}} className='w-full max-w-xl lg:w-3/4'>
+      <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0, x:130}} transition={{duration:1.2}} className='w-full max-w-xl lg:w-3/4'>
         <h6 className='mb-2 font-semibold '>{project.title}</h6>
         <p className='mb-4 text-neutral-400'>{project.description}</p>
         {project.technologies.map((tech,index)=>(
